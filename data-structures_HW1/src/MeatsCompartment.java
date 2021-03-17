2ef0b3edaf0690a363b290ed0332991c7952fe70
Yasar Polatlı <73195242+yelimot@users.noreply.github.com>
2021-03-17 20:05:47 +0300

    Tüm methodlar eklendi.

--- data-structures_HW1/src/MeatsCompartment.java
+++ data-structures_HW1/src/MeatsCompartment.java
@@ -5,24 +5,38 @@ public class MeatsCompartment<Item> implements IBag<Item>{
 	private int lastIndex;
 	private int MC_CAPACITY;
 	
-	public MeatsCompartment() {
+	public MeatsCompartment(int capacity) {
 		Item[] tmpBag = (Item[]) new Object[11];
 		MC = tmpBag;
 		lastIndex = 0;
-		MC_CAPACITY = 5000;
+		MC_CAPACITY = capacity;
+	}
+	
+	public int getMC_size() {
+		return MC_CAPACITY;
+	}
+
+	public void setMC_size(int MC_size) {
+		MC_CAPACITY = MC_size;
 	}
 	
 	public boolean add(Item newItem) {
 		boolean result = true;
-		if (isFull()) {
+		if (newItem.getWeight() > MC_CAPACITY) {
+			System.out.println("Item weight exceeds remaining capacity.");
 			result = false;
-			// warning message comes here.
+			}
+			
+		else if (isFull()) {
+			result = false;
+			System.out.println("Compartment is full.");
 			}
 		else {
 			MC[lastIndex] = newItem;
 			lastIndex++; 
-			// dolmadan önceki son zz
-			MC_CAPACITY -= newItem.getWeight();}
+			System.out.println(newItem.getName() + "is added to the basket.");			
+			MC_CAPACITY -= newItem.getWeight();
+			}
 		return result;
 		}
 	
@@ -31,16 +45,76 @@ public class MeatsCompartment<Item> implements IBag<Item>{
 		}
 	
 	public boolean isFull() {
-		// erken biterse deðiþkenden çýkar
-		return (MC_CAPACITY <= 499);
+		return (MC_CAPACITY <= 0);
+		}
+	
+	public Item removeByIndex(int index) {
+		Item result = null;
+		if(!isEmpty() && index >= 0) {
+			result= MC[index];
+			MC[index] = MC[lastIndex-1];
+			MC[lastIndex-1] = null;
+			lastIndex--;
+			}
+		return result;
+		}
+	
+	public Item remove() {
+		Item result = null;
+		if(lastIndex > 0) {
+			result = MC[lastIndex - 1];
+			MC[lastIndex - 1] = null;
+			lastIndex--;
+			}
+		return result;
+		}
+	
+	public Item remove(Item item) {
+		int index = getIndexOf(item);
+		Item result = removeByIndex(index);
+		return result;
+		}
+	
+	public int getIndexOf(Item item) {
+		int whereIsIt = -1;
+		boolean place = false;
+		int i = 0;
+		while(!place && (i < lastIndex)) {
+			if (item.equals(MC[i])) {
+				place = true;
+				whereIsIt = i;
+				}
+			i++;
+			}
+		return whereIsIt;
+		}
+	
+	public boolean contains(Item item) {
+		return (getIndexOf(item) > -1);
+		}
+	
+	public void displayItems() {
+		int i = 0;
+		while(MC[i] != null) {
+			if(!MC[i].equals(MC[i+1]))
+				System.out.println(MC[i].toString());
+			i++;
+			}
 		}
 
-	public int getMC_size() {
-		return MC_CAPACITY;
+	public void dump() {
+		while(!isEmpty()) {
+			remove();
+		}
 	}
-
-	public void setMC_size(int mC_size) {
-		MC_CAPACITY = mC_size;
+	
+	public boolean transferTo(IBag<Item> targetBag, Item item) {
+		if(this.contains(item)) {
+			targetBag.add(item);
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 }
