75d90ac087ff3fdd2a1e9b459f757f04f45adc1b
Yasar Polatlı <73195242+yelimot@users.noreply.github.com>
2021-03-17 20:07:21 +0300

    Baglar oluşturuldu, daha iş var.

--- data-structures_HW1/src/ShoppingApp.java
+++ data-structures_HW1/src/ShoppingApp.java
@@ -1,11 +1,19 @@
 import java.util.Scanner; 
+
 public class ShoppingApp {
 	public static void main(String[] args) {
 		
-		Item[] abc;
+		Item[] invBag;
 		
-		abc = FileIO.readInventory();
+		invBag = FileIO.readInventory();
 		System.out.println("ramo");
+		
+		IBag<Item> s_b = new ShoppingBasket(2000);
+		IBag<Item> m_c = new MeatsCompartment(5000);
+		IBag<Item> s_c = new SnacksCompartment(2000);
+		IBag<Item> b_c = new BevaragesCompartment(4000);
+		IBag<Item> vf_c = new VegetablesFruitsCompartment(3000);
+
 
 		while(true){
 			Scanner userInput = new Scanner(System.in);
@@ -13,8 +21,9 @@ public class ShoppingApp {
 			System.out.println("[1]Go to shopping");
 			System.out.println("[2]See status of the fridge");
 			System.out.println("[3]Exit");
-			int userOption = userInput.nextInt();  // Read user input
-    		if(userOption == 1){
+			int userOption1 = userInput.nextInt();  // Read user input
+			
+    		if(userOption1 == 1){
 				while(true){
 					System.out.println("Please select an option:");
 					System.out.println("[1]Add an item to basket");
@@ -22,13 +31,16 @@ public class ShoppingApp {
 					System.out.println("[3]Finish shopping");
 					int userOption2 = userInput.nextInt();  // Read user input
 					if(userOption2 == 1){
-						System.out.println("Please select an item:");
-						for (int i = 0; i < abc.length i++) {
-							System.out.println("[ " +i + " ] "+ abc[i].getName());
+						System.out.println("Please select an item: ");
+						for (int i = 0; i < invBag.length; i++) {
+							System.out.println("["+i+"] "+ invBag[i].getName());
 						}
 						int selectItem = userInput.nextInt();
+						
+						
+						// Stub waits operations.
 						// itemi seçince baskete ekleme işlemi yapılacak
-						// if basket == full ise break atılacak.
+						// if basket == full ise break.
 						}
 					else if(userOption2 == 2){
 						// prints basket 
@@ -40,6 +52,7 @@ public class ShoppingApp {
 
 					break;
 					}
+			
 			}
 			else if(userOption == 2){
 				// Remaning capatity'ler gösterilecek
