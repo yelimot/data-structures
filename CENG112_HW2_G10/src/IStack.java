6809d54c2caf09ce49124d5439308f758c9d5f9e
Yasar Polatlı <73195242+yelimot@users.noreply.github.com>
2021-04-01 21:44:26 +0300

    Delete CENG112_HW2_G10 directory

--- CENG112_HW2_G10/src/IStack.java
+++
@@ -1,9 +0,0 @@
-
-public interface IStack <T> {
-	public boolean isEmpty();
-    public boolean isFull();
-    public void push(T newEntry); // throws StackFullException;
-    public T pop(); // throws StackEmptyException;   
-    public T peek(); // throws StackEmptyException;    
-    public void clear();
-}
