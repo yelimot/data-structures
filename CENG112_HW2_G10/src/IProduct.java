a8d7f7f700f42b0ffafe65935349221618c3091b
Yasar Polatlı <73195242+yelimot@users.noreply.github.com>
2021-09-04 20:55:35 +0300

    Delete CENG112_HW2_G10 directory

--- CENG112_HW2_G10/src/IProduct.java
+++
@@ -1,9 +0,0 @@
-
-public interface IProduct {
-	
-	public String getName();
-	public void setName(String name);
-	public boolean isManufactured();
-	public boolean isStored();
-	public boolean isSold();
-}
